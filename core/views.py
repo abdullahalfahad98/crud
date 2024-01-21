0d493b6644388f411013db58ab1f774cc637d465
fahadkhan98 <fahadkhancsebd@gmail.com>
2024-01-22 02:11:41 +0600

    [Add]  Create Student Data

--- core/views.py
+++ core/views.py
@@ -7,5 +7,24 @@ def home(request):
 
 def AddStudent(request):
     if request.method== 'POST' or request.method== 'post':
-        print("*********")
+        print("***********")
+        print("Added")
+        roll= request.POST.get("roll")
+        name=request.POST.get("name")
+        email=request.POST.get("email")
+        phone=request.POST.get("address")
+        address=request.POST.get("address")
+    # else:
+    #     print("Not worked")
+        
+        s=Student()
+        s.roll = roll
+        s.name = name
+        s.email = email
+        s.phone = phone
+        s.address = address
+
+        s.save()
+
+        
     return render(request,'core/add.html', {})
